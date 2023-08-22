84171cb97e069136614025879afa32b602c54c6c
jcamiloparra7 <jcamiloparra7>
2023-08-22 21:25:29 +0000

    style: remove sql markups

--- 04 - Delta Live Tables/sql_delta_live_pipeline_test.sql
+++ 04 - Delta Live Tables/sql_delta_live_pipeline_test.sql
@@ -1,26 +1,24 @@
 -- Databricks notebook source
--- MAGIC %sql
--- MAGIC CREATE OR REFRESH LIVE TABLE clickstream_raw_sql
--- MAGIC COMMENT "The raw wikipedia clickstream dataset, ingested from /databricks-datasets."
--- MAGIC AS
--- MAGIC   SELECT
--- MAGIC     *
--- MAGIC   FROM
--- MAGIC     json.`/databricks-datasets/wikipedia-datasets/data-001/clickstream/raw-uncompressed-json/2015_2_clickstream.json`;
+CREATE OR REFRESH LIVE TABLE clickstream_raw_sql
+COMMENT "The raw wikipedia clickstream dataset, ingested from /databricks-datasets."
+AS
+  SELECT
+    *
+  FROM
+    json.`/databricks-datasets/wikipedia-datasets/data-001/clickstream/raw-uncompressed-json/2015_2_clickstream.json`;
 
 -- COMMAND ----------
 
--- MAGIC %sql
--- MAGIC CREATE OR REFRESH LIVE TABLE clickstream_prepared_sql(
--- MAGIC   CONSTRAINT valid_current_page EXPECT (current_page_title IS NOT NULL),
--- MAGIC   CONSTRAINT valid_count EXPECT (click_count > 0) ON VIOLATION FAIL UPDATE
--- MAGIC )
--- MAGIC COMMENT "Wikipedia clickstream data cleaned and prepared for analysis."
--- MAGIC AS SELECT
--- MAGIC   curr_title AS current_page_title,
--- MAGIC   CAST(n AS INT) AS click_count,
--- MAGIC   prev_title AS previous_page_title
--- MAGIC FROM live.clickstream_raw_sql;
+CREATE OR REFRESH LIVE TABLE clickstream_prepared_sql(
+  CONSTRAINT valid_current_page EXPECT (current_page_title IS NOT NULL),
+  CONSTRAINT valid_count EXPECT (click_count > 0) ON VIOLATION FAIL UPDATE
+)
+COMMENT "Wikipedia clickstream data cleaned and prepared for analysis."
+AS SELECT
+  curr_title AS current_page_title,
+  CAST(n AS INT) AS click_count,
+  prev_title AS previous_page_title
+FROM live.clickstream_raw_sql;
 
 -- COMMAND ----------
 
@@ -35,4 +33,4 @@ WHERE
   current_page_title = 'Apache_Spark'
 ORDER BY
   click_count DESC
-LIMIT 10
+LIMIT 10;
